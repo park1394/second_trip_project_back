4610482677d22e7647b0f488fad8da163184ad13
JOLDO <bbat115@gmail.com>
2026-04-17 13:31:44 +0900

    토큰 체크 필터 수정

--- second_trip_project_back/src/main/java/com/busanit401/second_trip_project_back/security/filter/TokenCheckFilter.java
+++ second_trip_project_back/src/main/java/com/busanit401/second_trip_project_back/security/filter/TokenCheckFilter.java
@@ -33,14 +33,6 @@ public class TokenCheckFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
 
-        // 요청 경로 가져오기
-        String path = request.getRequestURI();
-
-        if (path.startsWith("/api/member/register") || path.startsWith("/api/member/login")) {
-            filterChain.doFilter(request, response);
-            return;
-        }
-
         // 로그 출력
         log.info("Token Check Filter triggered...");
         log.info("JWTUtil instance: {}", jwtUtil);
@@ -115,11 +107,14 @@ public class TokenCheckFilter extends OncePerRequestFilter {
         String path = request.getRequestURI();
 
         // 아래 경로들로 들어오는 요청은 필터 로직(토큰 검사)을 건너뜁니다.
-        if (path.startsWith("/api/member/check-mid") ||
+        if (!path.startsWith("/api/") ||
+                path.startsWith("/api/member/check-mid") ||
+                path.startsWith("/api/member/register") ||
+                path.startsWith("/api/member/login") ||
                 path.startsWith("/api/member/check-email") ||
                 path.startsWith("/api/member/signup") ||
                 path.startsWith("/api/member/register") ||
-                path.startsWith("/api/car/") ||
+                path.startsWith("/rent/") ||
                 path.startsWith("/api/rental/search") ||
                 path.startsWith("/api/rental/unavailable") ||
                 path.startsWith("/swagger-ui") ||
